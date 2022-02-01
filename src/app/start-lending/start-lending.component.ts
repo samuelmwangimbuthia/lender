30948b6dfe75ee77400618d1c62971f67d53f51c
Samuel Mwangi Mbuthia <mbuthia.jsamuel@gmail.com>
2022-02-01 10:03:04 +0300

    capture details for motor vehicle and title deed

--- src/app/start-lending/start-lending.component.ts
+++ src/app/start-lending/start-lending.component.ts
@@ -16,30 +16,31 @@ export class StartLendingComponent implements OnInit {
 
     //set controls for all onboarding form input fields
     this.onboardingFormGroup = this.fb.group({
-      firstName: ['',[Validators.pattern, Validators.minLength(1)]],
-      lastName: ['',[Validators.pattern, Validators.minLength(1)]],
+      firstName: ['',[Validators.required, Validators.minLength(1)]],
+      lastName: ['',[Validators.required, Validators.minLength(1)]],
       phoneNumber: Number,
       email: ['', [Validators.required, Validators.email]],
       amount: Number,
-      collateral: '',
+      collateral: ''
     })
 
-
-this.populateLendersData();
+    //this.onboardingFormGroup.valueChanges.subscribe(value =>console.log(value))//
+    this.onboardingFormGroup.get('collateral').valueChanges.subscribe(value =>console.log(value))
+//this.populateLendersData();
 }
 
  //the method shall be reused by the borrower and lender component to prepopulate the form with
-  //with the saved data
+  /* with the saved data
 populateLendersData(){
-  this.onboardingFormGroup.setValue({
-    firstName: 'Samuel',
-      lastName: 'Mwangi',
+  this.onboardingFormGroup.patchValue({
+    firstName: 'Emmanuel',
+      lastName: 'Mbuthia',
       phoneNumber: 729782466,
       email: 'mbuthia.jsamuel@gmail.com',
-      amount: 20000,
-      collateral: 'logbook'
+      collateral: ''
   })
 }
+*/
 /*createNewLender(data){
   console.log(data)
 } */
@@ -47,4 +48,18 @@ createNewLender(data) {
   this.sendLenderData.emit(data)
 
 }
+
+// what happens when you click the radio button
+
+selectCollateral(security: string):void{
+  const collateralControl = this.onboardingFormGroup.get('collateral');
+  if (security === 'logbook'){
+    collateralControl.setValue('logbook')
+  } else if(security === 'titleDeed'){
+    collateralControl.setValue('titleDeed')
+  }else {
+    collateralControl.setValue('none')
+  }
+}
+
 }
